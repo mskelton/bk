67c94b83920fac4398ea75807ce5b5be016dd44a
Mark Skelton <mdskelton99@gmail.com>
2022-01-07 08:36:27 -0600

    Fix message

--- cmd/root.go
+++ cmd/root.go
@@ -19,6 +19,7 @@ var rootCmd = &cobra.Command{
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	err := rootCmd.Execute()
+
 	if err != nil {
 		os.Exit(1)
 	}
